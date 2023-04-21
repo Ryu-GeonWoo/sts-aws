ff999801ddbe94c182a8c2bd8edc827a16e0e5f4
Ryu. geonwoo <john2318@naver.com>
2023-04-21 19:28:39 +0900

    Initial commit

---
+++ src/main/java/com/example/stsaws/StsAwsApplication.java
@@ -0,0 +1,13 @@
+package com.example.stsaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/stsaws/StsAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stsaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StsAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
